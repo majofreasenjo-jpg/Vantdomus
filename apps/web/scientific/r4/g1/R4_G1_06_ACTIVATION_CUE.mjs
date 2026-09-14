aaf844c703ed7bd6fd4f8e4cd2f6455a02c6d532
majofreasenjo-jpg <majofreasenjo@gmail.com>
2026-09-14 15:39:12 -0300

    admit R4 G1 activation cue

---
+++ apps/web/scientific/r4/g1/R4_G1_06_ACTIVATION_CUE.mjs
@@ -0,0 +1,9 @@
+import { admitR4ProspectiveEntry } from "../../../lib/domiG5R4ProspectiveAdmission.mjs";
+
+export const R4_G1_ACTIVATION_CUE = admitR4ProspectiveEntry({
+  entryId: "R4-G1-0006-CUE",
+  observedAt: "2026-09-14T15:25:00-03:00",
+  role: "ACTIVATION_CUE",
+  semanticClass: "PAIR_TRIGGER_ALPHA",
+  contentToken: "R4C-4F7N-CUE",
+});
