804347cd137e56859dabfdba43bd79c058470870
majofreasenjo-jpg <majofreasenjo@gmail.com>
2026-09-14 15:38:42 -0300

    admit R4 G1 remote anchor

---
+++ apps/web/scientific/r4/g1/R4_G1_01_REMOTE_ANCHOR.mjs
@@ -0,0 +1,9 @@
+import { admitR4ProspectiveEntry } from "../../../lib/domiG5R4ProspectiveAdmission.mjs";
+
+export const R4_G1_REMOTE_ANCHOR = admitR4ProspectiveEntry({
+  entryId: "R4-G1-0001-ANCHOR",
+  observedAt: "2026-09-14T15:24:10-03:00",
+  role: "REMOTE_ANCHOR",
+  semanticClass: "PAIR_KEY_ALPHA",
+  contentToken: "R4A-7Q2M-ANCHOR",
+});
