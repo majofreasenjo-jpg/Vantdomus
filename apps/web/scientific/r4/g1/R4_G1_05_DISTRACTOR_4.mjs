eb8ccac57513368114ef039474989086987d329b
majofreasenjo-jpg <majofreasenjo@gmail.com>
2026-09-14 15:39:05 -0300

    admit R4 G1 distractor 4

---
+++ apps/web/scientific/r4/g1/R4_G1_05_DISTRACTOR_4.mjs
@@ -0,0 +1,9 @@
+import { admitR4ProspectiveEntry } from "../../../lib/domiG5R4ProspectiveAdmission.mjs";
+
+export const R4_G1_DISTRACTOR_4 = admitR4ProspectiveEntry({
+  entryId: "R4-G1-0005-DISTRACTOR",
+  observedAt: "2026-09-14T15:24:50-03:00",
+  role: "DISTRACTOR",
+  semanticClass: "NEUTRAL_EPSILON",
+  contentToken: "R4D-4-V9LM",
+});
