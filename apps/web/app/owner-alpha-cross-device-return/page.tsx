11aeaaa8c4822e305b4cf67a4f0665ec392106b8
majofreasenjo-jpg <majofreasenjo@gmail.com>
2026-09-15 16:47:53 -0300

    P5 add mobile-to-desktop return route

---
+++ apps/web/app/owner-alpha-cross-device-return/page.tsx
@@ -0,0 +1,5 @@
+import OwnerAlphaCrossDeviceReturnHarness from "./OwnerAlphaCrossDeviceReturnHarness";
+
+export default function OwnerAlphaCrossDeviceReturnPage() {
+  return <OwnerAlphaCrossDeviceReturnHarness />;
+}
